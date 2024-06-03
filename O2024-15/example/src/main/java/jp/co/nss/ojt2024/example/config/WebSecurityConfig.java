ecbf97642e7c8e3d0776f43221d877951853ba8f
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2024-06-04 08:02:27 +0900

    wip

--- O2024-15/example/src/main/java/jp/co/nss/ojt2024/example/config/WebSecurityConfig.java
+++ O2024-15/example/src/main/java/jp/co/nss/ojt2024/example/config/WebSecurityConfig.java
@@ -2,29 +2,21 @@ package jp.co.nss.ojt2024.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import jp.co.nss.ojt2024.example.service.CustomUserDetailsService;
+// import jp.co.nss.ojt2024.example.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private final CustomUserDetailsService userDetailsService;
+    // private final CustomUserDetailsService userDetailsService;
 
-    public WebSecurityConfig(CustomUserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
+    // public WebSecurityConfig(CustomUserDetailsService userDetailsService) {
+    //     this.userDetailsService = userDetailsService;
+    // }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -45,25 +37,25 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-                UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .build();
+    // @Bean
+    // public UserDetailsService userDetailsService() {
+    //             UserDetails user = User.builder()
+    //             .username("user")
+    //             .password(passwordEncoder().encode("password"))
+    //             .roles("USER")
+    //             .build();
 
-        return new InMemoryUserDetailsManager(user);
-    }
+    //     return new InMemoryUserDetailsManager(user);
+    // }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+    // @Bean
+    // public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+    //         throws Exception {
+    //     return authenticationConfiguration.getAuthenticationManager();
+    // }
 }
