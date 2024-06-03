27ecfcc916d11208fc26a9670b5307276af6036c
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2024-06-03 21:58:31 +0900

    add Hello, World

---
+++ O2024-15/example/src/main/java/jp/co/nss/ojt2024/example/HelloController.java
@@ -0,0 +1,21 @@
+package jp.co.nss.ojt2024.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    /**
+     * /helloリクエストの処理
+     * 
+     * @param model Model
+     * @return helloworld.htmlのパス
+     */
+    @GetMapping("/hello")
+    public String getHello(Model model) {
+        model.addAttribute("helloworld", "Hello, World");
+        return "helloworld";
+    }
+}
