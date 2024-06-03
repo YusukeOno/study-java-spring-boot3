44f441cedcbb82fe09c5c10409205a145bac558b
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2024-06-03 23:02:56 +0900

    wip

---
+++ O2024-15/example/src/main/java/jp/co/nss/ojt2024/example/config/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package jp.co.nss.ojt2024.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import jp.co.nss.ojt2024.example.service.CustomUserDetailsService;
+
+@Configuration
+public class WebSecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    public WebSecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.requestMatchers("/login").permitAll()
+                .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin.loginPage("/login").defaultSuccessUrl("/hello", true).permitAll())
+                .logout(logout -> logout.permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
