5f69feff9c36e278e39e503812f0ad7acb10041e
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2024-06-04 20:29:56 +0900

    formatting

--- O2024-15/example/src/main/java/jp/co/nss/ojt2024/example/config/WebSecurityConfig.java
+++ O2024-15/example/src/main/java/jp/co/nss/ojt2024/example/config/WebSecurityConfig.java
@@ -6,56 +6,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-// import jp.co.nss.ojt2024.example.service.CustomUserDetailsService;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    // private final CustomUserDetailsService userDetailsService;
-
-    // public WebSecurityConfig(CustomUserDetailsService userDetailsService) {
-    //     this.userDetailsService = userDetailsService;
-    // }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/login").permitAll() // /loginへのアクセスは認証不要
-                .anyRequest().authenticated() // それ以外のリクエストは認証を要求
-            )
-            .formLogin(formLogin -> formLogin
-                .loginPage("/login") // ログインページを設定
-                .defaultSuccessUrl("/hello", true) // 認証成功後にリダイレクトするURLを設定
-                .failureUrl("/login?error=true") // 認証失敗時のリダイレクトURL
-                .permitAll() // ログインページへのアクセスは許可
-            )
-            .logout(logout -> logout
-                .permitAll() // ログアウトページへのアクセスを許可
-            );
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login").permitAll() // /loginへのアクセスは認証不要
+                        .anyRequest().authenticated() // それ以外のリクエストは認証を要求
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login") // ログインページを設定
+                        .defaultSuccessUrl("/hello", true) // 認証成功後にリダイレクトするURLを設定
+                        .failureUrl("/login?error=true") // 認証失敗時のリダイレクトURL
+                        .permitAll() // ログインページへのアクセスは許可
+                )
+                .logout(logout -> logout
+                        .permitAll() // ログアウトページへのアクセスを許可
+                );
         return http.build();
     }
-
-    // @Bean
-    // public PasswordEncoder passwordEncoder() {
-    //     return new BCryptPasswordEncoder();
-    // }
-
-    // @Bean
-    // public UserDetailsService userDetailsService() {
-    //             UserDetails user = User.builder()
-    //             .username("user")
-    //             .password(passwordEncoder().encode("password"))
-    //             .roles("USER")
-    //             .build();
-
-    //     return new InMemoryUserDetailsManager(user);
-    // }
-
-    // @Bean
-    // public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-    //         throws Exception {
-    //     return authenticationConfiguration.getAuthenticationManager();
-    // }
 }
