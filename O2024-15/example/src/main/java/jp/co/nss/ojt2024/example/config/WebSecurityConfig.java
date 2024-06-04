2d1452b63abde1bd3d44a59a304612ab414cc2f3
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2024-06-04 20:32:40 +0900

    formatting

--- O2024-15/example/src/main/java/jp/co/nss/ojt2024/example/config/WebSecurityConfig.java
+++ O2024-15/example/src/main/java/jp/co/nss/ojt2024/example/config/WebSecurityConfig.java
@@ -10,22 +10,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/login").permitAll() // /loginへのアクセスは認証不要
-                        .anyRequest().authenticated() // それ以外のリクエストは認証を要求
-                )
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login") // ログインページを設定
-                        .defaultSuccessUrl("/hello", true) // 認証成功後にリダイレクトするURLを設定
-                        .failureUrl("/login?error=true") // 認証失敗時のリダイレクトURL
-                        .permitAll() // ログインページへのアクセスは許可
-                )
-                .logout(logout -> logout
-                        .permitAll() // ログアウトページへのアクセスを許可
-                );
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests(authorize -> authorize
+                                                .requestMatchers("/login").permitAll() // /loginへのアクセスは認証不要
+                                                .anyRequest().authenticated() // それ以外のリクエストは認証を要求
+                                )
+                                .formLogin(formLogin -> formLogin
+                                                .loginPage("/login") // ログインページを設定
+                                                .defaultSuccessUrl("/hello", true) // 認証成功後にリダイレクトするURLを設定
+                                                .failureUrl("/login?error=true") // 認証失敗時のリダイレクトURL
+                                                .permitAll() // ログインページへのアクセスは許可
+                                )
+                                .logout(logout -> logout
+                                                .permitAll() // ログアウトページへのアクセスを許可
+                                );
+                return http.build();
+        }
 }
